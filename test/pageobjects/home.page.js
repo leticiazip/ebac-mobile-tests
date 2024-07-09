28efc672d2d54dbd779c06d8fdd1adeaf8bc3aa6
Letícia Castilho <leticiasoares30@gmail.com>
2024-07-09 00:45:32 -0300

    Upando projeto

---
+++ test/pageobjects/home.page.js
@@ -0,0 +1,7 @@
+import { $ } from '@wdio/globals'
+
+class HomePage extends Page {
+
+}
+
+export default new LoginPage();
---
+++ test/specs/login.test.js
@@ -0,0 +1,8 @@
+import { expect, driver } from '@wdio/globals'
+
+describe('My Login application', () => {
+    it('should login with valid credentials', async () => {
+        await driver.pause(10000)
+    })
+})
+
